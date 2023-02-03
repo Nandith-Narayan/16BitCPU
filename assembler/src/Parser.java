2cc31920c2d48fed30ae0febe52743fcdcb67b01
Nandith Narayan <nandith2ndemail@gmail.com>
2023-02-02 20:50:20 -0500

    Added file handling

--- assembler/src/Parser.java
+++ assembler/src/Parser.java
@@ -1,2 +1,34 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import parser.AssemblyLexer;
+import parser.AssemblyParser;
+import parser.AssemblyVisitor;
+
+import java.io.IOException;
+
 public class Parser {
+
+    public Parser(String path){
+        try {
+            // Load assembly file
+            CharStream cs = CharStreams.fromFileName(path);
+            // Tokenize file
+            AssemblyLexer lexer = new AssemblyLexer(cs);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            // create the Parse Tree
+            AssemblyParser parser = new AssemblyParser(tokens);
+            ParseTree parseTree = parser.program();
+
+            // Visit the nodes of the Parse Tree
+            Visitor visitor = new Visitor();
+            visitor.visit(parseTree);
+
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
 }
--- assembler/src/Visitor.java
+++ assembler/src/Visitor.java
@@ -1,2 +1,65 @@
-public class Visitor{
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import parser.AssemblyParser;
+import parser.AssemblyVisitor;
+
+import javax.xml.crypto.Data;
+
+public class Visitor implements AssemblyVisitor<DataWrapper> {
+    @Override
+    public DataWrapper visitProgram(AssemblyParser.ProgramContext ctx) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitStatement(AssemblyParser.StatementContext ctx) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitSimple_instruction(AssemblyParser.Simple_instructionContext ctx) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitCompound_instruction(AssemblyParser.Compound_instructionContext ctx) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitLabel(AssemblyParser.LabelContext ctx) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitLiteral(AssemblyParser.LiteralContext ctx) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitVariable(AssemblyParser.VariableContext ctx) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visit(ParseTree parseTree) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitChildren(RuleNode ruleNode) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitTerminal(TerminalNode terminalNode) {
+        return null;
+    }
+
+    @Override
+    public DataWrapper visitErrorNode(ErrorNode errorNode) {
+        return null;
+    }
 }
