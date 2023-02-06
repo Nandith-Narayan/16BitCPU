79677b1f3cb6b7ded8fafd840fa6a1161d606699
Nandith Narayan <nandith2ndemail@gmail.com>
2023-02-05 23:34:22 -0500

    loaded control logic EEPROMs in the simulation

--- scripts/generate_decoder_EEPROM_data.py
+++ scripts/generate_decoder_EEPROM_data.py
@@ -25,7 +25,9 @@ RESET_MICRO_STEP = 16
 
 def main():
     bt = time.time()
-    rom_data = []
+    rom_data_1 = []
+    rom_data_2 = []
+    rom_data_3 = []
     for address in range(2**12):
         opcode = address & 0x03F
         if opcode < len(OPCODES):
@@ -38,16 +40,39 @@ def main():
         carry_flag = (address >> 11) & 0x01
         
         bits_to_enable = compute_control_step(opcode, step, zero_flag, carry_flag)
-        
-        
+        b1, b2, b3 = make_control_words(bits_to_enable)
+        rom_data_1.append(b1)
+        rom_data_2.append(b2)
+        rom_data_3.append(b3)
+
     at = time.time()
     print(f"Generated Decoder EEPROM data in {(at-bt)*1000:.1f} ms.")
     
-    output = bytes(rom_data) 
-    with open("DECODER_ROM_DATA.bin", "wb") as f:
+    output = bytes(rom_data_1) 
+    with open("DECODER_ROM_DATA_1.bin", "wb") as f:
+        f.write(output)
+    output = bytes(rom_data_2) 
+    with open("DECODER_ROM_DATA_2.bin", "wb") as f:
+        f.write(output)
+    output = bytes(rom_data_3)
+    with open("DECODER_ROM_DATA_3.bin", "wb") as f:
         f.write(output)
         
-    print(f"Wrote {len(output)} bytes.")
+    print(f"Wrote {len(output)}x3 bytes.")
+
+def make_control_words(enable_bits):
+    control_word = [0]*24
+    for bit in enable_bits:
+        control_word[bit] = 1
+
+    
+    control_word = ("".join([str(i) for i in control_word]))    
+    byte_1 = int(control_word[:8][::-1])
+    byte_2 = int(control_word[8:16][::-1])
+    byte_3 = int(control_word[16:24][::-1])
+        
+        
+    return (byte_1&0x0FF, byte_2&0x0FF, byte_3&0x0FF)
 
 def compute_control_step(opcode, step, zero_flag, carry_flag):
     if step == 0: 
@@ -56,7 +81,7 @@ def compute_control_step(opcode, step, zero_flag, carry_flag):
         return [RAM_OUT, INST_IN, PC_ENABLE]
         
     if opcode == "NOP":
-        return []
+        return [RESET_MICRO_STEP]
     if opcode == "ADDA":
         return ADDA(step)
     if opcode == "ADDB":
@@ -65,11 +90,62 @@ def compute_control_step(opcode, step, zero_flag, carry_flag):
         return SUBA(step)
     if opcode == "SUBB":
         return SUBB(step)
-        
+    if opcode == "XOR":
+        return XOR(step)
+    if opcode == "OR":
+        return OR(step)
+    if opcode == "AND":
+        return AND(step)
+    if opcode == "ADDI":
+        return ADDI(step)
+    if opcode == "ADD":
+        return ADD(step)
     
     print(f"OPCODE: {opcode} not implemented")
     
     return []
+
+def ADD(step):
+    if step == 2:
+        return [PC_OUT, RAM_ADDR]
+    if step == 3:
+        return [RAM_OUT, RAM_ADDR, PC_ENABLE]
+    if step == 4:
+        return [RAM_OUT, B_IN]
+    if step == 5:
+        return [ALU_OUT, A_IN]
+        
+    return [RESET_MICRO_STEP]     
+    
+    
+def ADDI(step):
+    if step == 2:
+        return [PC_OUT, RAM_ADDR]
+    if step == 3:
+        return [RAM_OUT, B_IN, PC_ENABLE]
+    if step == 4:
+        return [ALU_OUT, A_IN]
+        
+    return [RESET_MICRO_STEP]  
+
+def AND(step):
+    if step == 2:
+        return [ALU_OP2, ALU_OUT, A_IN]
+        
+    return [RESET_MICRO_STEP]  
+    
+def XOR(step):
+    if step == 2:
+        return [ALU_OP3, ALU_OUT, A_IN]
+        
+    return [RESET_MICRO_STEP]
+    
+def OR(step):
+    if step == 2:
+        return [ALU_OP1, ALU_OP2, ALU_OUT, A_IN]
+        
+    return [RESET_MICRO_STEP]     
+    
     
 def ADDA(step):
     if step == 2:
